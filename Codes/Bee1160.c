04bd0052da0e5a24280288daa0d64a7dff3eee8b
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-01-28 16:15:27 +0600

    This is the easy 1160 number problem solved

---
+++ Codes/Bee1160.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    while (T--) {
+        int PA, PB, years = 0;
+        double G1, G2;
+
+        scanf("%d %d %lf %lf", &PA, &PB, &G1, &G2);
+
+        while (PA <= PB && years <= 100) {
+            PA += PA * G1 / 100.0;
+            PB += PB * G2 / 100.0;
+            years++;
+        }
+
+        if (years > 100)
+            printf("Mais de 1 seculo.\n");
+        else
+            printf("%d anos.\n", years);
+    }
+
+    return 0;
+}
+
