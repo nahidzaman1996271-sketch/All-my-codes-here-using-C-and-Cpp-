9f3991df2b842fbbb0de1e039da176578fc88dfc
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-01-29 14:08:36 +0600

    Just trying

--- Codes/Practice for c++/main.cpp
+++ Codes/Practice for c++/main.cpp
@@ -1,46 +1,15 @@
 #include <iostream>
-#include <conio.h>
 using namespace std;
-int main()
-{
-    int i,pos,val,n=5;
-    int arr[10]={10,20,30,40,50};
-    cout << "Elements in the array: " << endl <<endl;
-    for(i=0; i<n; i++){
-        cout << "Elements in index " << i << " is " << arr[i]<<endl;
-    }
-    cout << endl;
-    cout << "Enter the position you want to enter the value: ";
-    cin >> pos;
-    cout << endl;
-    cout << "Enter the value you want to insert: ";
-    cin >> val;
-    cout << endl;
-    arr[pos] = val;
-    for(i = n-1; i>pos; i--){
-        arr[i+1]=arr[i];
-    }
-    n = n+1;
-    cout << "Values after insertion: "<<endl;
-        for(i=0; i<n; i++){
-        cout << "Elements in index " << i << " is " << arr[i]<<endl;
-    }
-    cout << endl;
-    cout << "Total elements after insertion: "<< n <<endl;
-    cout << endl;
-    cout << "The position you want to delete: ";
-    cin >> pos;
-    val = arr[pos];
-    for(i = pos; i<n;i++){
-        arr[i]=arr[i+1];
-    }
-    n = n-1;
-    cout << "The elements after deletion: "<<endl <<endl;
-    cout << "The value is deleted is: " << val << endl << endl;
-    for(i=0; i<n; i++){
-        cout << "Elements in index " << i << " is " << arr[i]<<endl;
-    }
-    cout <<endl;
-    cout << "Total elements in the array is: " << n;
-    getch();
+int main(){
+int i,arr[10];
+for(i=0;i<10;i++){
+    cin>>arr[i];
+}
+for(i=0;i<10;i++){
+        if(arr[i]<=0){
+            arr[i]=1;
+        }
+        cout << "X["<<i<<"]" <<" = " <<arr[i]<<endl;
+
+}
 }
