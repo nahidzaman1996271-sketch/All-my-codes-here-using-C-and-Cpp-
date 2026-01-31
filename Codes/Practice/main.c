fb5d3bfe161c13579e118fb6b75e4967020e5206
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-02-01 01:10:54 +0600

    Just changed a little

--- Codes/Practice/main.c
+++ Codes/Practice/main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
-int main(){
-for(int i=1;i<5;i++){
-    printf("%d+",i);
-}
-printf("5");
+int main()
+{
+    int n, pos, i ,value;
+    int arr[10]={10,20,30,40,50};
+    n = 5;
+    printf("The elements of the array is: \n\n");
+    for( i = 0 ; i < n; i++){
+        printf("The elements of index %d is: %d\n",i , arr[i]);
+    }
+    printf("\n");
+printf("Enter the position or index where you want to insert new value: ");
+scanf("%d", &pos);
 printf("\n");
-for(int i=1;i<=5;i++){
-    printf("%d ",i);
+printf("Enter the value: ");
+scanf("%d",&value);
+printf("\n");
+for (i = n-1;i>= pos; i--){
+    arr[i+1]=arr[i];
 }
+arr[pos] = value;
+n = n+1;
+printf("Array after insertion: \n\n");
+for( i = 0 ; i < n; i++){
+        printf("Elements at index %d is: %d\n",i,arr[i]);
+    }
+    printf("\nThe total length is: %d",n);
+    return 0;
 }
-
