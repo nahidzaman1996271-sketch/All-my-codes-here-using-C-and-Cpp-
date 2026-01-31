734d9060d3791df4452335a6010584c372827418
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-02-01 01:07:41 +0600

    just doing another cp

--- Codes/Practice/main.c
+++ Codes/Practice/main.c
@@ -1,55 +1,12 @@
 #include <stdio.h>
 int main(){
-int n;
-printf("How many elements you want to enter: ");
-scanf("%d",&n);
-int A[n],f=0,x=0,m;
-printf("Enter the elements: ");
-for(int i=0; i<n;i++){
-    scanf("%d",&A[i]);
+for(int i=1;i<5;i++){
+    printf("%d+",i);
 }
-int low=0,high = n-1,mid;
-if(A[0]<A[1]){
-    printf("Enter number to search: ");
-    scanf("%d",&m);
-    while(low<=high){
-        mid = (high+low)/2;
-        if(A[mid]==m){
-                printf("Element found at %d\n",mid+1);
-            f=1;
-            break;
-        }
-        else if(A[mid]<m){
-            low = mid+1;
-        }
-        else {
-            high = mid-1;
-        }
-    }
-    if(f==0)printf("Element not found.\n");
+printf("5");
+printf("\n");
+for(int i=1;i<=5;i++){
+    printf("%d ",i);
 }
-else{
-        printf("Enter number to search: ");
-    scanf("%d",&m);
-    while(low<=high){
-        mid = (high+low)/2;
-        if(A[mid]==m){
-                   printf("Element found at %d\n",mid+1);
-            f=1;
-            break;
-        }
-        else if(A[mid]<m){
-                 high = mid-1;
-
-        }
-        else {
-             low = mid+1;
-        }
-    }
-
-    if(f==0)printf("Element not found.\n");
-
-}
-return 0;
 }
 
