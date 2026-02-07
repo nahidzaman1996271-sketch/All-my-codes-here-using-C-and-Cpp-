db59e5d86f247eb86f45b047a17abca1675654a4
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-02-07 13:47:58 +0600

    wow this one is actually cool

---
+++ Codes/Bee1179.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int par[5],impar[5], evencount=0,oddcount=0,num;
+    for(int i=0; i<15; i++){
+        cin >> num;
+        if(num%2==0){
+            if(evencount==5){
+                for(int j=0;j<5;j++){
+                    cout << "par[" << j <<"] = " << par[j] << endl;
+                    evencount=0;
+                }
+            }
+            par[evencount++] = num;
+
+        } else {
+                        if(oddcount==5){
+                for(int j=0;j<5;j++){
+                    cout << "impar[" << j <<"] = " << impar[j] << endl;
+                    oddcount=0;
+                }
+            }
+            impar[oddcount++] = num;
+
+        }
+    }
+    /// print the remaining odd numbers
+    for(int k=0; k<oddcount;k++){
+         cout << "impar[" << k <<"] = " << impar[k] << endl;
+    }
+    /// print the remaining even numbers
+     for(int k=0; k<evencount;k++){
+         cout << "par[" << k <<"] = " << par[k] << endl;
+    }
+    return 0;
+}
