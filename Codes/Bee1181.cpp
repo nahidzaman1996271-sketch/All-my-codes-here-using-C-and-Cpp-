e519150091cfafacacb5bb354d5ff891dfd1aa52
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-02-08 23:05:21 +0600

    Before going to sleep

---
+++ Codes/Bee1181.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int L,i,j,sum=0;
+    float M[12][12],avg=0;
+    char T;
+    cin >> L;
+    cin.ignore();
+    cin >> T;
+    for(i=0;i<12;i++){
+        for(j=0;j<12;j++){
+            cin >> M[i][j];
+        }
+    }
+     for(i=0;i<12;i++){
+            sum+=M[L][i];
+     }
+     avg = sum / 12.0;
+if(T == 'S'){
+    cout << fixed << setprecision(1)<< sum <<  endl;
+
+}
+else {
+    cout << fixed << setprecision(1)<< avg <<  endl;
+}
+return 0;
+}
