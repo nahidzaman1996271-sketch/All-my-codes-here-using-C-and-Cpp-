4c95efd9924a6c59a663f46eaee50cd40bbc33e0
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-02-22 19:33:42 +0600

    It still can't accept in Bee and i dunno why?

--- Codes/Bee1185.cpp
+++ Codes/Bee1185.cpp
@@ -16,7 +16,7 @@ int main()
 
     for(int i = 0; i < 12; i++){
         for(int j = 0; j < 12; j++){
-            if(j > i){   // above main diagonal
+            if(j > i){   /// above main diagonal
                 sum += M[i][j];
             }
         }
