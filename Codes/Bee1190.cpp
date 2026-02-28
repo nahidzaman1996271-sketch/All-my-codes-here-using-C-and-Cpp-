d69378686181d206efe83f0a0c154acf2f2f2b69
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-02-28 22:31:10 +0600

    just solved one

---
+++ Codes/Bee1190.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    double M[12][12],sum=0,avg;
+    int ct=0,x=1,y=10;
+    char O;
+    cin >> O;
+    for(int i=0;i<12;i++){
+        for(int j=0;i<12;j++){
+            cin >> M[i][j];
+        }
+    }
+       for(int i=11;i>=7;i--){
+        for(int j=x;i<=y;j++){
+               sum += M[j][i];
+            ct++;
+}
+            x++;
+            y--;
+       }
+       avg = sum / (double)ct;
+       if(O=='S'){
+            cout << fixed << setprecision(1) << sum << endl;
+       }
+       else {
+        cout << fixed << setprecision(1) << avg << endl;
+       }
+       return 0;
+}
