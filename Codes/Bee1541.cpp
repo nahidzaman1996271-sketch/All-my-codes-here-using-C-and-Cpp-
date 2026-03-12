09773d3940afc41171e7eb658221e0a31e02da45
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-03-13 00:13:17 +0600

    last one for today

---
+++ Codes/Bee1541.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+int A,B,C;
+while(cin>>A){
+    if(A==0)break;
+    cin >> B >> C;
+    int houseArea = A*B;
+    double landArea = houseArea / (C/100.0);
+    int x = sqrt(landArea);
+    cout << x <<  endl;
+
+}
+   return 0;
+}
