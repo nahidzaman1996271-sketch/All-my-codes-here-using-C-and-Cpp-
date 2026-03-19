49df6cc116d8d0a2d459589f3f0ceee3a2c996dc
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-03-19 21:22:25 +0600

    ha ha so much easy this one

---
+++ Codes/Bee1589.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m,n,t;
+    int sum =0;
+    cin >> t;
+    while(t--){
+        cin >> m >> n;
+        sum = m+n;
+        cout << sum << endl;
+    }
+    return 0;
+}
