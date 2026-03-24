822e15c356628b2b57c3f0371ff50a1b41723735
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-03-25 01:05:13 +0600

    this one is funny

---
+++ Codes/Bee1847.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    while (cin >> a >> b >> c) {
+
+        if (a > b && b <= c) cout << ":)" << endl;
+
+        else if (a < b && b >= c) cout << ":(" << endl;
+
+        else if (a < b && b < c && (c - b) < (b - a)) cout << ":(" << endl;
+
+        else if (a < b && b < c && (c - b) >= (b - a)) cout << ":)" << endl;
+
+        else if (a > b && b > c && (a - b) > (b - c)) cout << ":)" << endl;
+
+        else if (a > b && b > c && (a - b) <= (b - c)) cout << ":(" << endl;
+
+        else if (a == b) {
+            if (b < c) cout << ":)" << endl;
+            else cout << ":(" << endl;
+        }
+    }
+
+    return 0;
+}
