9e8fc70afff0a6f241cea0d9029b07c57159e669
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-04-20 18:37:14 +0600

    just done some practice

--- Codes/Practice/main.c
+++ Codes/Practice/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
-int main()
-{
-
-printf("Helloo Guyzzzzz");
-return 0;
+#include <string.h>
+#define MAX 100
+ 
+char stack[MAX];
+int top = -1;
+ 
+void push(char c) { stack[++top] = c; }
+char pop() { return stack[top--]; }
+ 
+int main() {
+    char str[MAX], reversed[MAX];
+    printf("Enter a string: ");
+    scanf("%s", str);
+ 
+    int len = strlen(str);
+    for (int i = 0; i < len; i++) push(str[i]);
+    for (int i = 0; i < len; i++) reversed[i] = pop();
+    reversed[len] = '\0';
+ 
+    printf("Original String: %s\n", str);
+    printf("Reversed String: %s\n", reversed);
+ 
+    return 0;
 }
