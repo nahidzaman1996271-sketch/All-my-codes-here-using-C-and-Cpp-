61bf9e9e941ee8818c3b4960467e51053ff89ef0
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-05 15:05:26 +0600

    learning another things

--- Codes/Codes using C and C++/vector.cpp
+++ Codes/Codes using C and C++/vector.cpp
@@ -4,18 +4,27 @@
 #include<deque>
 #include<queue>
 #include<map>
+#include<unordered_set>
+#include<algorithm>
+#include<set>
 using namespace std;
+bool comparator(pair<int, int>p1, pair<int, int> p2){
+    if(p1.second < p2.second) return true;
+    if(p1.second > p2.second) return false;
+
+       }
 int main()
 {
+    /* vector<int>vec = {1,2,3,4,5};
+    reverse(vec.begin(),vec.end());
+    for(auto val: vec){
+        cout << val << " ";
+    }
+    cout << endl;
+    */
 
-  map<string,int>m;
-  m["tv"] = 100;
-  m["laptop"] = 100;
-  m["headphones"] = 50;
-  m["tablet"] = 120;
-  m["watch"] = 50;
-  for(auto p: m){
-    cout << p.first << " " << p.second << endl;
-  }
-  return 0;
+    string s = "abc";
+    next_permutation(s.begin(),s.end());
+    cout << s << end;
+    return 0;
 }
