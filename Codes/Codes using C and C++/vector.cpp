4b16b9e39745b04b0bec4e10e7e4a847960110dd
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-05 15:28:30 +0600

    this STL functions are needed to practice more

--- Codes/Codes using C and C++/vector.cpp
+++ Codes/Codes using C and C++/vector.cpp
@@ -8,23 +8,13 @@
 #include<algorithm>
 #include<set>
 using namespace std;
-bool comparator(pair<int, int>p1, pair<int, int> p2){
-    if(p1.second < p2.second) return true;
-    if(p1.second > p2.second) return false;
-
-       }
 int main()
 {
-    /* vector<int>vec = {1,2,3,4,5};
-    reverse(vec.begin(),vec.end());
-    for(auto val: vec){
-        cout << val << " ";
-    }
-    cout << endl;
-    */
 
-    string s = "abc";
-    next_permutation(s.begin(),s.end());
-    cout << s << end;
-    return 0;
+
+int a = 5;
+int b = 9;
+swap(a,b);
+cout <<"a= " << a << endl;
+return 0;
 }
