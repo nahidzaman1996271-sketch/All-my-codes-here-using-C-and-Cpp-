ad77cd62c666c72118c071860444610fdcb57e7f
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-06-01 23:23:57 +0600

    just make the codes fresh

--- Codes/leetcode/28.Find_the_Index_of_the_First_Occurrence_in_a_String.cpp
+++ Codes/leetcode/28.Find_the_Index_of_the_First_Occurrence_in_a_String.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-      int n = haystack.size(); 
+      int n = haystack.size();
       int m = needle.size();
 
       if(m==0){
         return 0;
       }
       if(n<m){
-        return -1; 
+        return -1;
       }
       for(int i=0; i<n; i++){
         if(haystack[i]==needle[0]){
@@ -18,5 +18,5 @@ public:
         }
       }
       return -1;
-    } 
+    }
 };
