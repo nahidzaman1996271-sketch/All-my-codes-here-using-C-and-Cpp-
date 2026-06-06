2c0509eaa7d3f0f51fb175f31491f56b2f7b5996
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-06-06 21:15:48 +0600

    no skip for today solved another leetcode as usual

---
+++ Codes/leetcode/168.Excel_Sheet_Column_Title.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+    
+    string result = "";
+    
+    while (columnNumber>0)
+    {
+        columnNumber--;
+        char letter = 'A' + columnNumber % 26;
+        result = letter + result;
+        columnNumber /= 26;
+    }
+    return result;
+    }
+};
