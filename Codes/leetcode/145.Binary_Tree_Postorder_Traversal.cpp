fb7096b227e7e97f53bdeda224200131b14a4bc9
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-06-12 00:12:50 +0600

    and this is it for today and i was also related to some questions that we guy's are solved today hope you guy's will see that this is it for today good night for you guy's until the next update hope you guys will be safe and sound see ya bye bye

--- Codes/leetcode/145.Binary_Tree_Postorder_Traversal.cpp
+++ Codes/leetcode/145.Binary_Tree_Postorder_Traversal.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
+    void pot(vector<int> &ans,TreeNode* root){
+        if(root == NULL){
+            return;
+        }
+        pot(ans, root->left);
+        pot(ans, root->right);
+        ans.push_back(root->val);
+    }
     vector<int> postorderTraversal(TreeNode* root) {
-        
+        vector<int> ans;
+        pot(ans,root);
+        return ans;
     }
 };
