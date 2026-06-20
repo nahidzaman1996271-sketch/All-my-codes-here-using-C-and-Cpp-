b43e901d2d0d02c1a49c6548bfad2b50bea3668f
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-06-20 20:54:19 +0600

    just summing of two inputs using pointer

---
+++ Codes/Codes using C and C++/sum_of_two_numbers_using_pointers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Input two values: ");
+    scanf("%d %d", &a,&b); // taking two inputs
+    int *ptr1 = &a; // *ptr stores the address of a
+    int *ptr2 = &b; // *ptr stores the address of b
+    printf("The sum of two variables input using pointer: %d\n", (*ptr1+*ptr2));
+    return 0;
+}
