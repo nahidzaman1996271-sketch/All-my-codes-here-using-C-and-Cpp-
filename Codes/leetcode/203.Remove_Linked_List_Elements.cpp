480463629a78fff2801269c36e2f0d16fbcb4236
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-06-24 22:41:14 +0600

    and this is the problem we solve today with 0ms runtime speed

--- Codes/leetcode/203.Remove_Linked_List_Elements.cpp
+++ Codes/leetcode/203.Remove_Linked_List_Elements.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        
+        while(head && head->val == val){
+            head=head->next;
+        }
+        ListNode *cur = head;
+        ListNode *pre = NULL;
+
+        while(cur!=NULL){
+            if(cur->val == val){
+                pre->next = cur->next;
+                cur = cur->next;
+            }
+            else{
+                pre = cur;
+                cur = cur->next;
+            }
+        }
+        return head;
     }
 };
