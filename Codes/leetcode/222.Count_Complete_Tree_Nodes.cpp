077d35a770e6fc906d9d39f87a5adaba7c8d1c63
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-06-29 23:59:52 +0600

    and here is the full code

--- Codes/leetcode/222.Count_Complete_Tree_Nodes.cpp
+++ Codes/leetcode/222.Count_Complete_Tree_Nodes.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int countNodes(TreeNode* root) {
+        if (root == NULL) return 0;
+        int leftH = 0;
+        int rightH = 0;
+        TreeNode* pleft = root;
+        TreeNode* pright = root;
+        while (pleft != NULL) {
+            pleft = pleft->left;
+            leftH++;
+        }
+        while (pright != NULL) {
+            pright = pright->right;
+            rightH++;
+        }
+        if (leftH == rightH)
+            return (1 << leftH) - 1;
         
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
