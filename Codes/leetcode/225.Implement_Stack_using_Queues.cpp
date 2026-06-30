0e3e6673dc14b5a7e9fd9220b0649cfc36ca3551
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-06-30 22:48:40 +0600

    now we are going to solve this for today

---
+++ Codes/leetcode/225.Implement_Stack_using_Queues.cpp
@@ -0,0 +1,22 @@
+class MyStack {
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        
+    }
+    
+    int pop() {
+        
+    }
+    
+    int top() {
+        
+    }
+    
+    bool empty() {
+        
+    }
+};
