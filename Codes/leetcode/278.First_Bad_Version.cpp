4792d057f7ba01cc6aa7139c0a919b3c03358680
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-07-10 23:32:27 +0600

    and here is the solve of today's question

---
+++ Codes/leetcode/278.First_Bad_Version.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long int low = 1;
+        long int high = n;
+        long int = mid;
+        long int result = n;
+
+        while (long<=high)
+        {
+            mid = (low+high)/2;
+            if(isBadVersion(mid)){
+                result = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return result;
+        
+    }
+};
