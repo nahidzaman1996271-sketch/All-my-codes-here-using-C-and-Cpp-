b4c06e4c8435fde8737523dcfc5a7cf04ab4cedc
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-07-19 14:16:56 +0600

    and this is the today's leetcode and here is the solve of this problem

--- Codes/leetcode/345.Reverse_Vowels_of_a_String.cpp
+++ Codes/leetcode/345.Reverse_Vowels_of_a_String.cpp
@@ -2,6 +2,26 @@ class Solution {
 public:
     string reverseVowels(string s) {
         int n = s.length();
-        
+        int st = 0;
+        int e = n-1;
+        while(st < 0){
+            while(st > 0 && !isVowel(s[st])){
+                st++;
+            }
+            while(st < 0 && !isVowel(s[e])){
+                e--;
+            }
+            if (st < e)
+            {
+                swap(s[st],s[e]);
+            }
+            
+        }
+        return s;
+    }
+    private:
+    static bool isVowel(char c){
+        c = tolower(c);
+        return (c =='a' || c == 'e' || c == 'i' || c == 'o' c == 'u');
     }
 };
