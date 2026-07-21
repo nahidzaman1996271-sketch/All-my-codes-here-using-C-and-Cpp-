1feab55bf29e9143cb75bfcec138c71dd527728a
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-07-21 23:58:42 +0600

    and this is the code for today's leetcode bye bye tataaaaaaa..

--- Codes/leetcode/350.Intersection_of_Two_Arrays_II.cpp
+++ Codes/leetcode/350.Intersection_of_Two_Arrays_II.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        
+        unordered_map<int,int> counts;
+        vector<int> res;
+        for(int num : nums1){
+            counts[num]++;
+        }
+
+        for(int num: nums2){
+            if(counts[num]>0){
+                res.push_back(num);
+                counts[num]--;
+            }
+        }
+        return res;
     }
 };
