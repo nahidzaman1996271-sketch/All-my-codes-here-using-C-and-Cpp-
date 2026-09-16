0f09d40d5fc59b833bac52e4edab6bf6153e2029
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-09-16 23:22:25 +0600

    just changed a little bit

--- Codes/java practices/practice.java
+++ Codes/java practices/practice.java
@@ -1,16 +1,23 @@
+```java
 import java.util.Scanner;
 
 public class practice {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Give me some input: ");
         int n = sc.nextInt();
-        while(n < 0 ){
-            System.out.print("choose the input number greater than 0");
+
+        while (n < 0) {
+            System.out.print("Choose an input number greater than 0: ");
+            n = sc.nextInt();
         }
-        for(int i=1; i<=n; i++){
+
+        for (int i = 1; i <= n; i++) {
             System.out.println(i);
-        
         }
+
+        sc.close();
     }
 }
+```
