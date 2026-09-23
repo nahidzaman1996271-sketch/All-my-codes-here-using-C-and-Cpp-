61435dfc6c45e18c0d2ca6c6d75a470d59439aa6
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-09-23 21:34:05 +0600

    so this abstraction in Java

---
+++ Codes/java practices/Abstraction and Polymorphism/Demo.java
@@ -0,0 +1,46 @@
+public class Demo {
+    public static void main(String[] args){
+        Car car = new ElectricCar();
+
+        car.start();
+        car.accelerate();
+        car.brake();
+    }
+}
+
+abstract class Car {
+
+    void start(){
+        System.out.println("Car started");
+    }
+
+    abstract void accelerate();
+
+    abstract void brake();
+}
+
+class FuelCar extends Car {
+
+    @Override
+    void accelerate(){
+        System.out.println("Fuel car is accelerating");
+    }
+
+    @Override
+    void brake(){
+        System.out.println("Fuel car is braking");
+    }
+}
+
+class ElectricCar extends Car {
+
+    @Override
+    void accelerate(){
+        System.out.println("Electric car is accelerating");
+    }
+
+    @Override
+    void brake(){
+        System.out.println("Electric car is braking");
+    }
+}
